e422960f1305adc1a7364a3476b71535c212e0f6
Brendan Heberlein <bheberlein@wisc.edu>
2024-03-31 18:40:24 -0500

    [TMP] Add renumbering utility
    If raw data files are accidentally numbered with zero-based indexing, this utility can be used to renumber the input archives  with one-based indexing.

--- utils/repair.sh
+++ utils/repair.sh
@@ -22,3 +22,35 @@ repair () {
     echo "Input data archive OK!"
   fi
 }
+
+# Renumber flightlines from sessions mislabeled with zero-based indexing
+renumber () {
+  echo "Renumbering flightlines for session: $1"
+  DATA_DIRECTORY=$STAGING/data/raw/$1
+  # Iterate over flightlines in reverse sorted order
+  for f in $(ls $DATA_DIRECTORY/*.tar.gz | sort -r); do
+    # Parse flightline basename
+    ARCHIVE=${f##*/}
+    FLIGHTLINE=${ARCHIVE%%.*}
+    # Increment line number
+    printf -v LINE '%02d' $(echo "${FLIGHTLINE##*_} + 1" | bc -l)
+    # Corrected flightline basename
+    RENAMED=${SESSION}_$LINE
+    
+    echo "  $FLIGHTLINE  ->  $RENAMED"
+    
+    TEMPORARY_DIRECTORY=$DATA_DIRECTORY/$RENAMED
+    mkdir -p $TEMPORARY_DIRECTORY
+    tar -xzf $f -C $TEMPORARY_DIRECTORY
+    
+    for g in $TEMPORARY_DIRECTORY/${FLIGHTLINE}_*; do
+      mv $g ${g/$FLIGHTLINE/$RENAMED}
+    done
+    
+    cd $TEMPORARY_DIRECTORY
+    tar -czf $TEMPORARY_DIRECTORY.tar.gz *.{hyspex,hdr,txt}
+    mv $f ${f}.mislabeled
+    cd - > /dev/null
+    rm -r $TEMPORARY_DIRECTORY
+  done
+}
