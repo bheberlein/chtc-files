326174c9283d11eb021a030e13a119686d146954
Brendan Heberlein <bheberlein@wisc.edu>
2024-11-07 14:21:54 -0600

    [R/F] Edits to `utils/config.sh`

--- utils/config.sh
+++ utils/config.sh
@@ -7,25 +7,35 @@ resolve_config () {
   #  try to resolve automatically
   if [ -z ${CONFIG+x} ]; then
     
-    # A session-level config file has the same basename as the session
-    #  & is located in a session subdirectory
-    SESSION_CONFIG=$SESSION/${SESSION}_Config.json
+    # A flightline-level config file is located in a session subdirectory
+    #  & uses the full flightline basename, i.e. SITE-NAME_YYYYMMDD/SITE-NAME_YYYYMMDD_XX_Config.json
+    FLIGHTLINE_CONFIG=$SESSION/${FLIGHTLINE}_Config.json
+
+    # A session-level config file has the same basename as the session, i.e. SITE-NAME_YYYYMMDD_Config.json
+    SESSION_CONFIG=${SESSION}_Config.json
     
-    # A season-level config file basename just has the minimal site name & year
+    # A season-level config file basename just has the minimal site name & year, i.e. SITE-NAME_YYYY_Config.json
     # NOTE: Site name is stripped of trailing dash-separated numbers
     SEASON_CONFIG=${NAME%%-[0-9]}_${ISODATE:0:4}_Config.json
-    
-    # First look for session-level config
-    if [[ -f $CONFIG_DIR/${SESSION_CONFIG} ]]; then
+
+    # A project-level config file basename is just the project name, i.e. PROJECT_Config.json
+    PROJECT_CONFIG=${PROJECT}_Config.json
+
+    # First look for flightline-level config
+    if [[ -f $CONFIG_DIR/${FLIGHTLINE_CONFIG} ]]; then
+      CONFIG=$FLIGHTLINE_CONFIG;
+
+    # If not found, look for session-level config
+    elif [[ -f $CONFIG_DIR/${SESSION_CONFIG} ]]; then
       CONFIG=$SESSION_CONFIG;
     
     # If not found, look for season-level config
     elif [[ -f $CONFIG_DIR/${SEASON_CONFIG} ]]; then
       CONFIG=$SEASON_CONFIG;
     
-    # If a separate project basename is given, use that
-    elif [[ -n "${PROJECT+x}" ]]; then
-      CONFIG=${PROJECT}_Config.json;
+    # Finally, if a separate project basename is given, check for project-level config
+    elif [[ ( -n "${PROJECT+x}" ) && ( -f $CONFIG_DIR/${PROJECT_CONFIG} ) ]]; then
+      CONFIG=$PROJECT_CONFIG;
     
     else
       echo "ERROR: No configuration file found!" 1>&2
